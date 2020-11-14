bb7ea8f196d8bb8fa67e7f5801a3aa5d86f536ab
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-11-14 21:01:02 +0200

    Added simple parallax

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const copyPromoCode = document.querySelector('.js-promo-code')
+  const parallaxScene = document.querySelector('.js-parallax')
+
+  const pictureBgFront = document.querySelector('.js-front')
+  const pictureBgBack = document.querySelector('.js-back')
+  const witcher = document.querySelector('.js-middle')
 
   copyPromoCode.addEventListener('click', copy)
+  parallaxScene.addEventListener('mousemove', mouseMoveHandler)
+  parallaxScene.addEventListener('mouseleave', mouseLeaveHandler)
 
   function copy(e) {
     if (e.target.tagName == 'BUTTON') {
@@ -12,5 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
       this.classList.add('copied')
       setTimeout(() => this.classList.remove('copied'), 1000)
     }
+  }
+
+  function mouseMoveHandler(e) {
+    pictureBgBack.style.transform  = `translate(${-e.offsetX*0.02}px, ${-e.offsetY*0.02}px)`
+    witcher.style.transform        = `translate(${e.offsetX*0.04}px, ${e.offsetY*0.04}px)`
+    pictureBgFront.style.transform = `translate(${e.offsetX*0.06}px, ${e.offsetY*0.06}px)`
+  }
+
+  function mouseLeaveHandler() {
+    pictureBgBack.style.transform  = `translate(0px, 0px)`
+    witcher.style.transform        = `translate(0px, 0px)`
+    pictureBgFront.style.transform = `translate(0px, 0px)`
   }
 })
