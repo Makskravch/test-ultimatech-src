993bf40f1c7e81c487425e7ed49d71bebe67c373
Maksym Kravchuk <kravchukmaxym@gmail.com>
2020-11-14 11:54:58 +0200

    Added a copy hint

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+  const copyPromoCode = document.querySelector('.js-promo-code')
+
+  copyPromoCode.addEventListener('click', copy)
+
+  function copy(e) {
+    if (e.target.tagName == 'BUTTON') {
+      const input = this.querySelector('input')
+      input.select()
+      document.execCommand('copy')
+      this.classList.add('copied')
+      setTimeout(() => this.classList.remove('copied'), 1000)
+    }
+  }
+})
